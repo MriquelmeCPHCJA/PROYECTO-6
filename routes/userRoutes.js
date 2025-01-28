396f4a1606dce67c6077ad79742151bc1a1e5263
Marcelo Riquelme <mriquelme@correo.com>
2025-01-28 17:33:54 -0300

    refactor: Comment

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,12 +10,12 @@ const auth = require('../middleware/authorization');
 
 const userRoutes = express.Router();
 
-userRoutes.post('/register', createUser); // localhost:3001/api/users/create-user
+userRoutes.post('/register', createUser); // localhost:3000/api/users/register
 
-userRoutes.post('/login', loginUser); // localhost:3001/api/users/login
+userRoutes.post('/login', loginUser); // localhost:3000/api/users/login
 
-userRoutes.get('/verify-user', auth, verifyUser); // localhost:3001/api/users/verify-user
+userRoutes.get('/verify-user', auth, verifyUser); // localhost:3000/api/users/verify-user
 
-userRoutes.put('/update-user/:id', updateUserById); // localhost:3001/api/users/update-user/:id
+userRoutes.put('/update-user/:id', updateUserById); // localhost:3000/api/users/update-user/:id
 
 module.exports = userRoutes;
