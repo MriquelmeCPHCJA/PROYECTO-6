f07d68c1ddc34ea41201651764d241076c2a8fb5
Marcelo Riquelme <mriquelme@correo.com>
2025-01-31 14:26:35 -0300

    refactor: modify categoryProduct Schema

--- models/CategoryProducts.js
+++ models/CategoryProducts.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const categoryProductsSchema = mongoose.Schema({
     name: {
         type: String,
+        unique: true,
         required: true,
         trim: true
     },
